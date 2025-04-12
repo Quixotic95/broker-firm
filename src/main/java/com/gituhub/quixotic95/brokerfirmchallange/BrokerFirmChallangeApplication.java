3e886cdf2515ffa07707cd85052ae8b1bad637e8
Emre <e.oguz95@hotmail.com>
2025-04-12 11:43:13 +0300

    Initial commit. Let's start!

---
+++ src/main/java/com/gituhub/quixotic95/brokerfirmchallange/BrokerFirmChallangeApplication.java
@@ -0,0 +1,13 @@
+package com.gituhub.quixotic95.brokerfirmchallange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrokerFirmChallangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrokerFirmChallangeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gituhub/quixotic95/brokerfirmchallange/BrokerFirmChallangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gituhub.quixotic95.brokerfirmchallange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrokerFirmChallangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
